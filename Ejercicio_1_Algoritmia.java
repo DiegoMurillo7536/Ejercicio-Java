e86463af32ac4b697d9cf0dc051742b4901ab6cc
Diego Alexander Murillo  Suárez <89222624+DiegoMurillo7536@users.noreply.github.com>
2022-04-23 22:58:39 -0500

    Hasta Ejercicio 67

--- Ejercicio_1_Algoritmia.java
+++ Ejercicio_1_Algoritmia.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.time.*;
 public class Ejercicio_1_Algoritmia {
 	static Scanner lapiz= new Scanner(System.in);
 	
@@ -87,6 +88,60 @@ public class Ejercicio_1_Algoritmia {
 			break;
 		case 30:
 			Ejercicio_30();
+			break;
+		case 32:
+			Ejercicio_32();
+			break;
+		case 34:
+			Ejercicio_34();
+			break;
+		case 36:
+			Ejercicio_36();
+			break;
+		case 38:
+			Ejercicio_38();
+			break;	
+		case 40:
+			Ejercicio_40();
+			break;
+		case 42:
+			Ejercicio_42();
+			break;	
+		case 44:
+			Ejercicio_44();
+			break;
+		case 47:
+			Ejercicio_47();
+			break;
+		case 49:
+			Ejercicio_49();
+			break;
+		case 51:
+			Ejercicio_51();
+			break;
+		case 53:
+			Ejercicio_53();
+			break;
+		case 55:
+			Ejercicio_55();
+			break;
+		case 57:
+			Ejercicio_57();
+			break;
+		case 59:
+			Ejercicio_59();
+			break;
+		case 61:
+			Ejercicio_61();
+			break;
+		case 63:
+			Ejercicio_63();
+			break;
+		case 65:
+			Ejercicio_65();
+			break;
+		case 67:
+			Ejercicio_67();
 			break;
 		default:
 			break;
@@ -451,8 +506,410 @@ public class Ejercicio_1_Algoritmia {
 		}
 	}
 	
+	static void Ejercicio_32(){
+		int p=lapiz.nextInt();
+		int q=lapiz.nextInt();
+		double expresion=(Math.pow(p, 3)+Math.pow(q,4))-(2*Math.pow(p,2));
+		if (expresion>680) {
+			System.out.println("El valor de p es:"+p);
+			System.out.println("El valor de q es:"+q);
+		} else {
+			System.out.println("El resultado es menor a 680");
+		}
+	}
+	
+	static void Ejercicio_34(){
+		System.out.println("Ingrese su sueldo actual");
+		int sueldo=lapiz.nextInt();
+		System.out.println("Elija la categoría a la que pertenece");
+		int categoria=lapiz.nextInt();
+		switch (categoria) {
+		case 1:
+			System.out.println("Su nuevo salario es de:"+(sueldo+(sueldo*0.15)));
+			break;
+		case 2:
+			System.out.println("Su nuevo salario es de:"+(sueldo+(sueldo*0.10)));
+			break;
+		case 3:
+			System.out.println("Su nuevo salario es de:"+(sueldo+(sueldo*0.08)));
+			break;
+		case 4:
+			System.out.println("Su nuevo salario es de:"+(sueldo+(sueldo*0.07)));
+			break;
+		default:
+			break;
+		}
+	}
+	
+	static void Ejercicio_36(){
+		System.out.println("Ingrese su temperatura en grados farenheit");
+		int temp=lapiz.nextInt();
+		if (temp>85) {
+		System.out.println("Hoy es un gran día para nadar");
+		} else if (temp<85 && temp>=70) {
+		System.out.println("Hoy es un gran día para practicar tennis");	
+		}else if (temp<70 && temp>=32) {
+		System.out.println("Hoy es un gran día para practicar golf");
+		}else if (temp<32 && temp>10) {
+		System.out.println("Hoy es un gran dia para esquiar");
+		}else if (temp<=10) {
+		System.out.println("Hoy es un gran día para hacer una marcha");
+		}
+
+		
+		}
+	
+	static void Ejercicio_38(){
+		System.out.println("Ingresa el día de tu nacimiento");
+		int dia=lapiz.nextInt();
+		System.out.println("Ingresa el mes de tu nacimiento");
+		int mes=lapiz.nextInt();
+		String signo;
+		if (mes==11 && (dia>=22 || dia<=31) || (mes==12 && (dia<=21))) {
+			signo="Sagitario";
+		System.out.println("SU signo es:"+signo);
+			
+		}else if (mes==12 && (dia>=22 || dia<=31) || (mes==1 && (dia<=20))) {
+			signo="Capricornio";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==1 && (dia>=21 || dia<=31) || (mes==2 && (dia<=19))) {
+			signo="Acuario";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==2 && (dia>=20 || dia<=28) || (mes==3 && (dia<=19))) {
+			signo="Picis";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==03 && (dia>=21 || dia<=31) || (mes==4 && (dia<=20))) {
+			signo="Aries";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==4 && (dia>=21 || dia<=31) || (mes==5 && (dia<=21))) {
+			signo="Tauro";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==5 && (dia>=22 || dia<=31) || (mes==6 && (dia<=21))) {
+			signo="Geminis";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==6 && (dia>=22 || dia<=31) || (mes==7 && (dia<=22))) {
+			signo="Cáncer";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==7 && (dia>=23 || dia<=31) || (mes==8 && (dia<=23))) {
+			signo="Leo";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==8 && (dia>=24 || dia<=31) || (mes==9 && (dia<=22))) {
+			signo="Virgo";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==9 && (dia>=23 || dia<=31) || (mes==10 && (dia<=22))) {
+			signo="Libra";
+			System.out.println("Su signo es:"+signo);
+		}else if (mes==10 && (dia>=23 || dia<=31) || (mes==11 && (dia<=21))) {
+			signo="Escorpión";
+			System.out.println("Su signo es:"+signo);
+		}
+	}
+	
+	static void Ejercicio_40(){
+	System.out.println("Escriba su lectura actual"); 
+	int lectura=lapiz.nextInt();
+	if (lectura>=0 && lectura <=100) {
+		System.out.println("El pago de este mes es de: 2622 COP");
+	}else if (lectura>100 && lectura<=300) {
+		System.out.println("EL pago de este mes es de: 7878 COP");
+	}else if (lectura>300 && lectura<=500) {
+		System.out.println("EL pago de este mes es de: 8952 COP");
+	}else if (lectura>500) {
+		System.out.println("EL pago de este mes es de: 9795 COP");
+	}
+	 
+	}
+	
+	static void Ejercicio_42(){
+	System.out.println("Ingrese su edad");
+	double edad=lapiz.nextInt();
+	System.out.println("Ingrese su nivel de hemoglobina");
+	double nivel_hemoglobina=lapiz.nextInt();
+	System.out.println("Indique su género a través de una M o F ");
+	String genero=lapiz.next();
+	if (edad==0.1 && (nivel_hemoglobina>=13 || nivel_hemoglobina<=26)) {
+	System.out.println("Su nivel es estable");
+	}else if ((edad>0.1 || edad<=0.6) && (nivel_hemoglobina>=10 || nivel_hemoglobina<=18)) {
+		System.out.println("Su nivel es estable");
+	}
+	else if ((edad>0.6 || edad<=1) && (nivel_hemoglobina>=11 || nivel_hemoglobina<=15)) {
+		System.out.println("Su nivel es estable");
+	}else if ((edad>1 || edad<=5) && (nivel_hemoglobina>=11.5 || nivel_hemoglobina<=15)) {
+		System.out.println("Su nivel es estable");
+	}else if ((edad>5 || edad<=10) && (nivel_hemoglobina>=12.6 || nivel_hemoglobina<=15.5)) {
+		System.out.println("Su nivel es estable");
+	}else if ((edad>10 || edad<=15) && (nivel_hemoglobina>=13 || nivel_hemoglobina<=15.5)) {
+		System.out.println("Su nivel es estable");
+	}else if ((edad>15 && genero=="F")  && (nivel_hemoglobina>=12 || nivel_hemoglobina<=16)) {
+		System.out.println("Su nivel es estable");
+	}else if ((edad>15 && genero=="M")  && (nivel_hemoglobina>=14 || nivel_hemoglobina<=18)) {
+		System.out.println("Su nivel es estable");
+	}else {
+		System.out.println("Su hemoglobina está rara,vaya al médico");
+	}
 	
 	}
+	
+	static void Ejercicio_44(){
+		System.out.println("De cuánto es la hipoteca?");
+		int valor_hipoteca=lapiz.nextInt();
+		if (valor_hipoteca<1000000) {
+			double inversion=valor_hipoteca*0.5;
+			double socio=valor_hipoteca*0.5;
+			System.out.println("Se hará una inversión de:"+(inversion+socio));
+		}else{
+			System.out.println("Se invirtió todo el dinero de la hipoteca");
+			double inversion=valor_hipoteca/2;
+			System.out.println("Se repartió "+inversion+" para cada uno");
+		}
+	}
+	
+	static void Ejercicio_47(){
+		for(int i=1;i<=100;i+=2)
+        {
+			if (i%7==0) {
+				System.out.println("");
+			}
+		System.out.println("Valor:"+i);
+        }	
+	}
+	
+	static void Ejercicio_49(){
+		int Pregunta_1=1;
+		int Pregunta_2=1;
+		int Pregunta_3=1;
+		if (Pregunta_1==1 && Pregunta_2==1 && Pregunta_3==1) {
+			System.out.println("Tuvo todas las respuestas correctas");
+		}else if (Pregunta_1==1 && Pregunta_2==1 && Pregunta_3==0) {
+			System.out.println("Tuvo las respuestas 1 y 2");
+		}else if (Pregunta_1==1 && Pregunta_3==1&& Pregunta_2==0) {
+			System.out.println("Tuvo las respuestas 1 y 3");
+		}else if (Pregunta_2==1 && Pregunta_3==1 && Pregunta_1==0) {
+			System.out.println("Tuvo las respuestas 2 y 3");
+		}else if (Pregunta_1==1 && Pregunta_2==0 && Pregunta_3==0) {
+			System.out.println("Tuvo la respuesta 1");
+		}else if (Pregunta_1==0 && Pregunta_2==1 && Pregunta_3==0) {
+			System.out.println("Tuvo las respuesta 2");
+		}else if (Pregunta_1==0 && Pregunta_2==0 && Pregunta_3==1) {
+			System.out.println("Tuvo las respuesta 3");
+		}else if (Pregunta_1==0 && Pregunta_2==0 && Pregunta_3==0) {
+			System.out.println("No tuvo ninguna respuesta correcta");
+		}	
+	}
+	
+	static void Ejercicio_51(){
+		for(int i=6;i<=402;i+=5)
+        {
+			int suma=+i;
+		System.out.println("Valor"+suma);
+
+        }
+	}
+	
+	static void Ejercicio_53(){
+		System.out.println("Ingrese su nombre");
+		String nombre=lapiz.next();
+		System.out.println("Ingrese su nacionalidad");
+		String nacionalidad=lapiz.next();
+		System.out.println("Ingrese su edad");
+		int edad=lapiz.nextInt();
+		System.out.println("Qué tipo de empleado es (1-3)");
+		int tipo_empleado=lapiz.nextInt();
+		System.out.println("Ingrese sus horas trabajadas");
+		int horas_trabajadas=lapiz.nextInt();
+		int sueldo=5000;
+		switch (tipo_empleado) {
+		case 1:
+		System.out.println("Nombre:"+nombre);
+		System.out.println("Nacionalidad:"+nacionalidad);
+		System.out.println("Edad:"+edad);
+		System.out.println("Horas trabajadas:"+horas_trabajadas);
+		System.out.println("Sueldo:"+sueldo);
+		if (sueldo>100000) {
+			System.out.println("Seguro social:"+(sueldo*0.03));
+		}
+			break;
+		case 2:
+		System.out.println("Nombre:"+nombre);
+		System.out.println("Nacionalidad:"+nacionalidad);
+		System.out.println("Edad:"+edad);
+		System.out.println("Horas trabajadas:"+horas_trabajadas);
+		System.out.println("Sueldo:"+sueldo);
+		if (sueldo>100000) {
+			System.out.println("Seguro social:"+(sueldo*0.03));
+		}
+			break;
+		case 3:
+			System.out.println("Nombre:"+nombre);
+			System.out.println("Nacionalidad:"+nacionalidad);
+			System.out.println("Edad:"+edad);
+			System.out.println("Horas trabajadas:"+horas_trabajadas);
+			System.out.println("Sueldo:"+sueldo);
+			if (sueldo>100000) {
+				System.out.println("Seguro social:"+(sueldo*0.03));
+			}
+				break;
+
+		default:
+			break;
+		}
+	}
+	
+	static void Ejercicio_55(){
+	System.out.println("Digita un número cualquiera");
+	int i=lapiz.nextInt();
+	int sumatoria=0;
+	int contador=i;
+	while (contador!=0) {
+		sumatoria=sumatoria+contador;
+		contador--;
+		
+	}
+	if (sumatoria>1000 || sumatoria<=900) {
+		System.out.println("Está alejado o se pasó del mil");
+		System.out.println("El resultado de "+i+ " es:"+sumatoria);
+
+	}else if(sumatoria<=1000)
+	System.out.println("Se ha acercado a mil");
+	System.out.println("El resultado de "+i+ " es:"+sumatoria);
+	}
+	
+	static void Ejercicio_57(){
+	System.out.println("Ingresa un número");
+	int n=lapiz.nextInt();
+	double x=0;
+	double rn=(((x+n)/x)/2);
+	for(x=0.1;rn>0;x++) {
+		if(rn>0){
+			System.out.println("La raiz cuadrada es:"+rn);
+			break;
+		}else{
+			System.out.println("Es un numero negativo");
+		}
+	   }
+	}
+	
+	static void Ejercicio_59(){
+	System.out.println("Ingrese su nombre");
+	String nombre=lapiz.next();
+	System.out.println("Ingrese su nota de matemática ");
+	int nota_mat=lapiz.nextInt();
+	System.out.println("Ingrese su nota de programacion ");
+	int nota_prg=lapiz.nextInt();
+	System.out.println("Ingrese su nota de inglés ");
+	int nota_ing=lapiz.nextInt();
+	String estado;
+	double promedio_final=(nota_ing+nota_mat+nota_prg)/3;
+	if (nota_mat<6 || nota_ing<6 || nota_prg<6) {
+		estado="Repobado";
+	}else {
+		estado="Aprobado";
+
+	}
+	System.out.println("Nombre:"+nombre+" Estado:"+estado+" Promedio final:"+promedio_final);
+		
+	}
+	
+	static void Ejercicio_61(){
+	System.out.println("Ingrese un número");
+	int multiplicador=lapiz.nextInt();
+	System.out.println("Ingrese otro número");
+	int multiplicando=lapiz.nextInt();
+	int i=0;
+	double resultado=0;
+		for (multiplicador=multiplicador; multiplicador!=0; i++) {
+			if (multiplicador%2!=0) {
+			
+			resultado=resultado+multiplicando;
+			}
+		multiplicando=multiplicando/2;
+		multiplicador=multiplicador*2;
+		System.out.println("El resultado de la multiplicación es:"+resultado);
+		
+		}
+	 
+	}
+	
+	static void Ejercicio_63(){
+		System.out.println("Ingresa tu edad");
+		int edad=lapiz.nextInt();
+		System.out.println("Escribe tu género");
+		String genero=lapiz.next();
+		System.out.println("Ingresa tu estado civil (Hombre o Mujer)");
+		String estado_c=lapiz.next();
+		
+		System.out.println("Edad:"+edad+" Genero:"+genero+" Estado Civil:"+estado_c);
+	}
+	
+	static void Ejercicio_65(){
+		String nombre="Diego murillo";
+		String ti="1025460660";
+		System.out.println("Ingrese su salario");
+		int sueldo_basico=lapiz.nextInt();
+		System.out.println("Ingrese la cantidad de hijos que tenga");
+		int cantidad_hijos=lapiz.nextInt();
+		System.out.println("Escriba sus inasistencias");
+		int inasistencias=lapiz.nextInt();
+		double aporte_hijo=sueldo_basico*(cantidad_hijos*0.10);
+		double caja_ahorros=sueldo_basico*0.1;
+		double seguro=sueldo_basico*0.02;
+		double aporte_inasistencias=0;
+		
+		if (inasistencias>2) {
+			 aporte_inasistencias=0;
+			} else{
+			 aporte_inasistencias=sueldo_basico*0.05;
+			}
+		double sueldo_neto=(sueldo_neto=sueldo_basico+aporte_hijo+aporte_inasistencias)-
+				caja_ahorros-seguro;
+		switch (sueldo_basico) {
+		case 100000:
+			System.out.println("Nombre:"+nombre+" Número de documento"+ti);
+			System.out.println("Sueldo básico:"+sueldo_basico);
+			System.out.println("Aporte a la caja de ahorros"+caja_ahorros);
+			System.out.println("Seguro social:"+seguro);
+			System.out.println("Tiene en total:"+sueldo_neto);
+			break;
+		
+		case 165000:
+			System.out.println("Nombre:"+nombre+" Número de documento"+ti);
+			System.out.println("Sueldo básico:"+sueldo_basico);
+			System.out.println("Aporte a la caja de ahorros"+caja_ahorros);
+			System.out.println("Seguro social:"+seguro);
+			System.out.println("Tiene en total:"+sueldo_neto);
+			break;
+		
+		case 250000:
+			System.out.println("Nombre:"+nombre+" Número de documento"+ti);
+			System.out.println("Sueldo básico:"+sueldo_basico);
+			System.out.println("Aporte a la caja de ahorros"+caja_ahorros);
+			System.out.println("Seguro social:"+seguro);
+			System.out.println("Tiene en total:"+sueldo_neto);
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	
+	static void Ejercicio_67(){
+	int deuda=12775;
+	int pago_1=100;
+	int pago_23=125;
+	int cuotas=14;
+	int ultimo_pago=1725;	
+	System.out.println("La persona debe pagar en las siguientes 10 deudas:"+(deuda-pago_1-pago_23-ultimo_pago)/cuotas);
+	}
+	
+	static void Ejercicio_69(){
+	int amigo_a;
+	}
+	
+	}
+	
+	
+	
 
 
 	
